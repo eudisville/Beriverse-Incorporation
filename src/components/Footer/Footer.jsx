70ed9d614cf03033c7eb84119d20e408f8042379
Eudes <mondesirkoffi30@gmail.com>
2024-11-27 08:54:08 +0000

    trois

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
   return (
     <div className='footer'>
       <div className="footer-left">
-        <h4>Beriverse &copy;2024</h4>
+        <h4>© 2024 Beriverse. All rights reserved.</h4>
       </div>
       <div className="footer-right">
         <Link to="mailto:beriverse@gmail.com" target='_blank'><img src={Mail} alt="" /></Link>
