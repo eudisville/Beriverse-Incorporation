e1346576ddc75be3ac147b70f467e8fc18cb4771
Eudes <mondesirkoffi30@gmail.com>
2025-01-28 14:19:45 +0000

    my

--- src/components/Home/Head.jsx
+++ src/components/Home/Head.jsx
@@ -3,14 +3,14 @@ import './Head.css'
 
 function Head({ title, description, background }) {
   return (
-    <header
+    <div className='header'
         style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${background})` }}
     >
         <div className="header-props">
             <h1>{title}</h1>
             <p>{description}</p>
         </div>
-    </header>
+    </div>
   )
 }
 
